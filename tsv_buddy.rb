ef1dc3b514f1fa90d8140eeb6ebd4184ac6492ca
unknown <rubyyu2800@gmail.com>
2021-10-11 03:38:59 +0800

    rubocop

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,28 @@
-# Module that can be included (mixin) to take and output TSV data
+# frozen_string_literal: true
+
+# Module that can be included (mixin) to take and output Yaml data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    newline = "\n"
+    tab     = "\t"
+    @data = []
+    content = tsv.split(newline).map { |line| line.split(tab) }
+    header = content.first
+    content.delete_at(0)
+    @data = content.map do |record|
+      record.each_with_index.map { |item, idx| [header[idx], item] }.to_h
+    end
   end
-
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
-  def to_tsv
 
+  def to_tsv
+    newline = "\n"
+    tab     = "\t"
+    header  = @data.first.keys.join(tab)
+    records = @data.map { |record| record.values.join(tab) }
+    ([header] + records).join(newline) + newline
   end
 end
